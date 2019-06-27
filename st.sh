9dce50770d3d55c6d4750e3f505f3f5af8c0f9d4
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-06-27 20:32:27 +0100

    batch

---
+++ st.sh
@@ -0,0 +1,11 @@
+# 27-6-2019 JHZ
+
+git add README.md
+git commit -m "README"
+git add schedule.md
+git commit -m "schedule"
+git add IF
+git commit -m "journal impact factor"
+git add st.sh
+git commit -m "batch"
+git push
