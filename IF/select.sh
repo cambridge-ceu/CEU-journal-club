e917d50ccf00093aca6118ccb546076f4c231b63
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-06-27 20:37:02 +0100

    journal impact factor

--- IF/select.sh
+++ IF/select.sh
@@ -2,5 +2,6 @@
 
 # working on .csv attachment by Stephen Bell
 
-head -1 *csv > select.txt
-ls *csv* | sed 's/.csv//g' | parallel "sed 's/,/\t/g' {}.csv | awk 'NR>1' | grep -v Copyright | grep -v exporting > {}.tsv"
+chmod -x csv/*
+head -1 csv/*csv > select.txt
+ls csv | sed 's/.csv//g' | parallel "sed 's/,/\t/g' csv/{}.csv | awk 'NR>1' | grep -v Copyright | grep -v exporting > {}.tsv"
