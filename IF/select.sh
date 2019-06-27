fa36e91039f1ef4ce9145379b66e9777395437d7
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-06-27 20:32:26 +0100

    journal impact factor

--- IF/select.sh
+++ IF/select.sh
@@ -1,2 +1,6 @@
+# 27-6-2017 JHZ
+
+# working on .csv attachment by Stephen Bell
+
 head -1 *csv > select.txt
 ls *csv* | sed 's/.csv//g' | parallel "sed 's/,/\t/g' {}.csv | awk 'NR>1' | grep -v Copyright | grep -v exporting > {}.tsv"
