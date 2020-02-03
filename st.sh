cb35cf06676e70976f812dd899a41a53fb57e826
jinghuazhao <jinghuazhao@hotmail.com>
2020-02-03 11:08:09 +0000

    batch

--- st.sh
+++ st.sh
@@ -1,11 +1,11 @@
-# 2-2-2020 JHZ
+# 3-2-2020 JHZ
 
 git add README.md
 git commit -m "README"
 git add schedule.md
 git commit -m "schedule"
-git add calendarview-1.2
-git commit -m "calendarview"
+git add calendarview-1.2 fullcalendar-4.3.1
+git commit -m "Calendar"
 git add IF
 git commit -m "journal impact factor"
 git add papers
