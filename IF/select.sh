24da0c8e253d98161efb058d2fab723a3ec577c4
jinghuazhao <jinghuazhao@hotmail.com>
2020-07-02 17:33:38 +0100

    journal impact factor

--- IF/select.sh
+++ IF/select.sh
@@ -1,7 +1,9 @@
-# 27-6-2017 JHZ
+# 2-7-2020 JHZ
 
 # working on .csv attachment by Steven Bell
 
 chmod -x csv/*
 head -1 csv/*csv > select.txt
 ls csv | sed 's/.csv//g' | parallel "sed 's/,/\t/g' csv/{}.csv | awk 'NR>1' | grep -v Copyright | grep -v exporting > {}.tsv"
+
+sed '127,128d;1,2d' csv/IF_2019_Statistics.csv | tr ',' '\t'> IF_2019_Statistics.tsv
