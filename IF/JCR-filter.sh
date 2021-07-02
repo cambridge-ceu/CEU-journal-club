f0f587d761bfd57ec70e3e243e822ad69fecc31a
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-07-02 22:40:46 +0100

    journal impact factor

--- IF/JCR-filter.sh
+++ IF/JCR-filter.sh
@@ -7,8 +7,7 @@ chmod -x csv/*
 ls csv | \
 xargs -l basename -s .csv | \
 parallel "
-   awk '/Rank|Journal name/,/Copyright/' csv/{}.csv | \
-   awk -vFS=',' -vOFS='\t' '!/^$/ && !/Copyright/' > {}.csv
+   awk '/Rank/,/Copyright/' csv/{}.csv | grep -v Copyright > {}.csv
 "
 #  awk -vFS=';' -vOFS='\t' '{gsub(/\t/,\"\",\$5);for (i=1;i<NF;i++) printf \$i OFS;print \$NF}' > {}.tsv
 #  sed -r ':L;s|\b(\"[0-9]+)\t([0-9]+\")\b|\1\2|g;t L' | \
