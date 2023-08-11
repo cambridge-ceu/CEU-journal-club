f919497ae38715696749650a0fe41a4e83b3c559
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2023-08-11 14:27:07 +0000

    Deployed 69975ab with MkDocs version: 1.1.2

---
+++ IF/csv/2021/2021.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+awk '/Journal name/,/^$/' CARDIAC\ \&\ CARDIOVASCULAR\ SYSTEMS.csv > ../2021-CARDIAC\ \&\ CARDIOVASCULAR\ SYSTEMS.csv
+awk '/Journal name/,/^$/' GENETICS\ \&\ HEREDITY.csv > ../2021-GENETICS\ \&\ HEREDITY.csv
+awk '/Journal name/,/^$/' STATISTICS\ \&\ PROBABILITY.csv > ../2021-STATISTICS\ \&\ PROBABILITY.csv
