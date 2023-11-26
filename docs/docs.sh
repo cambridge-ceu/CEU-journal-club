064f0c62aa716569dda1e6ba209159984f817d2c
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2023-11-26 21:47:26 +0000

    docs

--- docs/docs.sh
+++ docs/docs.sh
@@ -2,8 +2,8 @@
 
 function setup()
 {
-  module load python/3.7
-  source ~/COVID-19/py37/bin/activate
+  module load python/3.8
+  source ~/rds/public_databases/software/py38/bin/activate
 }
 
 setup
