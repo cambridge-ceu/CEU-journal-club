5cd129da5d9faa8c4b3d61efaaf478a9c85ff01a
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-06-21 15:58:53 +0100

    Deployed 917321e with MkDocs version: 1.5.3

--- docs.sh
+++ docs.sh
@@ -7,8 +7,8 @@ function setup()
 }
 
 if [ "$(uname -n | sed 's/-[0-9]*$//')" == "login-q" ]; then
-   module load ceuadmin/libssh/0.10.6-icelake
    module load ceuadmin/openssh/9.7p1-icelake
+   module load ceuadmin/libssh/0.10.6-icelake
 fi
 
 setup
